fd6a240c683931c7f4f237d7126c3c3f13a49356
Roney Santos <joseroneysilvasantos@gmail.com>
2021-06-14 08:02:08 -0700

    initial commit

---
+++ primero.py
@@ -0,0 +1,14 @@
+print('********************************')
+print('Bem vindo ao jogo de adivinhacao')
+print('********************************')
+
+numero_secreto = 90
+chute_str = input("Digite o seu numero: ")
+chute = int(chute_str)
+
+if (numero_secreto == chute):
+     print ("Acertou Miseravi")
+else:
+     print ("Errou Azalado")
+
+print ('Acabou essa porra de adivinhacao')
