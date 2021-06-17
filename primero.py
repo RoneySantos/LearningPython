09c2b91683b4f7f6888623fcbba5d5658237537e
Roney Santos <joseroneysilvasantos@gmail.com>
2021-06-16 22:10:47 -0300

    Update primero.py
    Primeiro while

--- primero.py
+++ primero.py
@@ -3,12 +3,17 @@ print('Bem vindo ao jogo de adivinhacao')
 print('********************************')
 
 numero_secreto = 90
-chute_str = input("Digite o seu numero: ")
-chute = int(chute_str)
+total_de_tentativas = 3
+rodada = 1
 
-if (numero_secreto == chute):
-     print ("Acertou Miseravi")
-else:
-     print ("Errou Azalado")
+while(rodada <= total_de_tentativas):
+     chute_str = input("Digite o seu numero: ")
+     chute = int(chute_str)
+
+     if (numero_secreto == chute):
+          print ("Acertou Miseravi")
+     else:
+          print ("Errou Azalado")
+     rodada = rodada + 1
 
 print ('Acabou essa porra de adivinhacao')
