f8e1582a2dac827fbc22fdadf443ab71bc32a5b5
Roney Santos <joseroneysilvasantos@gmail.com>
2021-06-16 22:13:04 -0300

    Update primero.py
    mensagem de quantidade de tentativa

--- primero.py
+++ primero.py
@@ -7,6 +7,7 @@ total_de_tentativas = 3
 rodada = 1
 
 while(rodada <= total_de_tentativas):
+     print('Tentativa', rodada, 'de', total_de_tentativas)
      chute_str = input("Digite o seu numero: ")
      chute = int(chute_str)
 
